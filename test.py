24451430a3d11105b75d838de7dcfd03b64a498a
Jessica Tong <66048432+eshentong@users.noreply.github.com>
2024-12-03 12:37:28 -0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Check if a specific secret exists
-if "google_credential" in st.secrets:
-    st.write("Secret 'google_credential' found!")
-else:
-    st.write("Secret 'google_credential' not found.")
-
-
